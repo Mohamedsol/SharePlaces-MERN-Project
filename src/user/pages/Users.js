56e4c319ac17ed02c11017686c746d0279fc7ce0
Mohamed <soltane.mohamed.achref@gmail.com>
2020-04-14 15:42:20 -0500

    Create Places and User Components

---
+++ src/user/pages/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import UserList from '../components/UsersList';
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+
+    const USERS = [
+
+    ]
+    return <UsersList items={USERS} />;
+}
+ 
+export default Users;
