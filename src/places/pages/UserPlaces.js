007fc57abfcf99573352c3f884388a437e437300
Mohamed <soltane.mohamed.achref@gmail.com>
2020-04-24 23:49:12 -0500

    connect the front end to the back end

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -29,6 +29,32 @@ const DUMMY_PLACES = [
       lng: -73.9878584
     },
     creator: 'u2'
+  },
+  {
+    id: 'p3',
+    title: 'Test1',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p4',
+    title: 'Test2',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
   }
 ];
 
